a24365ef7cca46681cde005efae8335de27af42a
TwinProduction <twin@twinnation.org>
2019-04-22 20:24:45 -0400

    Minor enhancement

--- color.go
+++ color.go
@@ -1,14 +1,16 @@
 package color
 
-var Reset = "\033[0m"
-var Red = "\033[31m"
-var Green = "\033[32m"
-var Yellow = "\033[33m"
-var Blue = "\033[34m"
-var Purple = "\033[35m"
-var Cyan = "\033[36m"
-var Gray = "\033[37m"
-var White = "\033[97m"
+var (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Purple = "\033[35m"
+	Cyan   = "\033[36m"
+	Gray   = "\033[37m"
+	White  = "\033[97m"
+)
 
 func Ize(color, message string) string {
 	return Colorize(color, message)
