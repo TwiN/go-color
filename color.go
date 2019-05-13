251aa53c093d28b64c53112c718763674e697234
TwinProduction <twin@twinnation.org>
2019-05-13 19:28:41 -0400

    Use custom type for colors for easier autocompletion

--- color.go
+++ color.go
@@ -1,21 +1,23 @@
 package color
 
+type Name string
+
 var (
-	Reset  = "\033[0m"
-	Red    = "\033[31m"
-	Green  = "\033[32m"
-	Yellow = "\033[33m"
-	Blue   = "\033[34m"
-	Purple = "\033[35m"
-	Cyan   = "\033[36m"
-	Gray   = "\033[37m"
-	White  = "\033[97m"
+	Reset  Name = "\033[0m"
+	Red    Name = "\033[31m"
+	Green  Name = "\033[32m"
+	Yellow Name = "\033[33m"
+	Blue   Name = "\033[34m"
+	Purple Name = "\033[35m"
+	Cyan   Name = "\033[36m"
+	Gray   Name = "\033[37m"
+	White  Name = "\033[97m"
 )
 
-func Ize(color, message string) string {
+func Ize(color Name, message string) string {
 	return Colorize(color, message)
 }
 
-func Colorize(color, message string) string {
-	return color + message + Reset
+func Colorize(color Name, message string) string {
+	return string(color) + message + string(Reset)
 }
