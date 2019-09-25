55ecb5a91629962b76942321a6b63e3d6ac35f3f
TwinProduction <twin@twinnation.org>
2019-09-24 23:49:09 -0400

    Add some tests + Add Github actions

--- color_test.go
+++ color_test.go
@@ -15,3 +15,15 @@ func TestOutput(t *testing.T) {
 	println(Red + "H" + Green + "el" + Purple + "lo" + Reset)
 	println(Ize(Red, "test"))
 }
+
+func TestColorize(t *testing.T) {
+	if Colorize(Red, "red") != "\033[31mred\033[0m" {
+		t.Fail()
+	}
+	if Colorize(Green, "green") != "\033[32mgreen\033[0m" {
+		t.Fail()
+	}
+	if Colorize(Blue, "blue") != "\033[34mblue\033[0m" {
+		t.Fail()
+	}
+}
